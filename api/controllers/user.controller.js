38bcb675663e35c37b6d1666959d08dad8f0eacb
Karem Saeed <95093124+karemsaeed20020@users.noreply.github.com>
2024-01-28 12:31:39 +0200

    create sign up api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,3 +1,5 @@
+import User from '../models/user.model.js';
+
 export const test = (req, res) => {
     res.json({
         message: "Api route is working"  
--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import userRouter from '../api/routes/user.route.js';
+import authRouter from '../api/routes/auth.route.js';
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -9,9 +10,11 @@ mongoose.connect(process.env.MONGO).then(() => {
 }).catch((error) => console.log(error));
 
 const app = express();
+app.use(express.json());
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000!");
 })
 
 app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
