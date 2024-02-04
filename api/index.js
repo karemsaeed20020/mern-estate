eda27fb8613e3f2e03eab9a1cb5ea1beeda22ab4
Karem Saeed <95093124+karemsaeed20020@users.noreply.github.com>
2024-02-04 21:52:55 +0200

    add create listing api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import userRouter from '../api/routes/user.route.js';
 import authRouter from '../api/routes/auth.route.js';
+import listingRouter from '../api/routes/listing.route.js';
 import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
 dotenv.config();
@@ -20,6 +21,7 @@ app.listen(3002, () => {
 
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
+app.use('/api/listing', listingRouter);
 
 app.use((err, req, res, next) => {
     const statuscode = err.statuscode || 500;
