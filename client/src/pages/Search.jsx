696815f5d7e0544b74f1215a2b86b4a9080ca1fe
Karem Saeed <95093124+karemsaeed20020@users.noreply.github.com>
2024-02-13 16:56:21 +0200

    add show more listings funtionality

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -111,6 +111,19 @@ const Search = () => {
         const searchQuery = urlParams.toString();
         navigate(`/search?${searchQuery}`);
       };
+      const onShowMoreClick = async () => {
+        const numberOfListings = listings.length;
+        const startIndex = numberOfListings;
+        const urlParams = new URLSearchParams(location.search);
+        urlParams.set('startIndex', startIndex);
+        const searchQuery = urlParams.toString();
+        const res = await fetch(`/api/listing/get?${searchQuery}`);
+        const data = await res.json();
+        if (data.length < 9) {
+          setShowMore(false);
+        }
+        setListings([...listings, ...data]);
+      };
     
   return (
     <div className="flex flex-col md:flex-row">
@@ -180,6 +193,9 @@ const Search = () => {
                         <ListingItem key={listing._id} listing={listing} />
                     ))
                 }
+                {showMore && (
+                    <button onClick={onShowMoreClick} className="text-green-700 hover:underline p-7 text-center w-full">Show more</button>
+                )}
             </div>
         </div>
     </div>
