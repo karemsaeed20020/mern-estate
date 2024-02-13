3b247418285a8a2ab27ab4c155fb6e65108b49f7
Karem Saeed <95093124+karemsaeed20020@users.noreply.github.com>
2024-02-13 23:14:12 +0200

    add pre installation

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import userRouter from '../api/routes/user.route.js';
 import authRouter from '../api/routes/auth.route.js';
 import listingRouter from '../api/routes/listing.route.js';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -11,6 +12,7 @@ mongoose.connect(process.env.MONGO).then(() => {
     console.log("Connected to MongoDB");
 }).catch((error) => console.log(error));
 
+const __dirname = path.resolve();
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
@@ -22,6 +24,11 @@ app.listen(3002, () => {
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 
 app.use((err, req, res, next) => {
     const statuscode = err.statuscode || 500;
